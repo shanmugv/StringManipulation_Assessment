5174a534c3766ad590dd461d31d7fca5798166ac
shanmuga <sundaramcseb@gmail.com>
2021-09-26 21:28:50 +0530

    Test method for spring boot

--- stringmanipulation/src/test/java/com/waitrose/stringmanipulation/StringManipulationApplicationTests.java
+++ stringmanipulation/src/test/java/com/waitrose/stringmanipulation/StringManipulationApplicationTests.java
@@ -1,13 +1,21 @@
 package com.waitrose.stringmanipulation;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StringManipulationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	private static final String[] ARGS = {};
 
+	@InjectMocks
+	private StringManipulationApplication stringManipulationApplication;
+
+	@Test
+	@DisplayName("verifying application starts")
+	void applicationStarts() throws Exception {
+		StringManipulationApplication.main(ARGS);
+	}
 }
