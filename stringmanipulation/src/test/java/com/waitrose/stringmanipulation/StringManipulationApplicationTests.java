2a3c18609bf1bd1136b4392e8de177be738f3107
shanmuga <sundaramcseb@gmail.com>
2021-09-27 10:54:24 +0530

    Test method for string manipulation

--- stringmanipulation/src/test/java/com/waitrose/stringmanipulation/StringManipulationApplicationTests.java
+++ stringmanipulation/src/test/java/com/waitrose/stringmanipulation/StringManipulationApplicationTests.java
@@ -1,9 +1,15 @@
 package com.waitrose.stringmanipulation;
 
+import com.waitrose.exception.NoNumericValueException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StringManipulationApplicationTests {
@@ -18,4 +24,42 @@ class StringManipulationApplicationTests {
 	void applicationStarts() throws Exception {
 		StringManipulationApplication.main(ARGS);
 	}
+
+	@Test
+	public void testStringFunction() throws Exception {
+		final BigInteger value = stringManipulationApplication.stringFunction("100klh564abc365bg");
+		assertEquals(BigInteger.valueOf(564), value,
+				"Verify the expected result from String Function");
+	}
+
+	@Test
+	public void testNoNumericValueInStringFunction() throws Exception {
+		Throwable exception = assertThrows(NoNumericValueException.class, () -> stringManipulationApplication.stringFunction("klhgjycxvkhkhjabbg"));
+		assertEquals("No numeric value is present in string", exception.getMessage());
+	}
+
+	@Test
+	public void testNoValuesPresentFunction() {
+		Throwable exception = assertThrows(NoNumericValueException.class, () -> stringManipulationApplication.stringFunction(""));
+		assertEquals("the given String is empty", exception.getMessage());
+	}
+
+	@Test
+	public void testOnlyIntegerInStringFunction() throws Exception {
+		String numStr = "187264817241642";
+		BigInteger bigInteger = new BigInteger(numStr);
+		final BigInteger value = stringManipulationApplication.stringFunction(numStr);
+		assertEquals(bigInteger, value,
+				"Verify the expected result from String Function");
+	}
+
+	@Test
+	public void testOnlyIntegerZeroInStringFunction() throws Exception {
+
+		BigInteger bigIntegerZero = BigInteger.ZERO;
+		final BigInteger value = stringManipulationApplication.stringFunction("0");
+		assertEquals(bigIntegerZero, value,
+				"Verify the expected result from String Function");
+	}
+
 }
